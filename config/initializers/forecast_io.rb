cc9bed4d4bedf28f4d1cc4c20b59b5832abe2e89
Jason Hinchcliffe <hinchy@gmail.com>
2016-07-28 05:01:26 +0000

    removed SQL3lite and added env vars

--- config/initializers/forecast_io.rb
+++ config/initializers/forecast_io.rb
@@ -1,5 +1,9 @@
 require 'forecast_io'  
 
+#ForecastIO.configure do |config|    
+#  config.api_key = Figaro.env.forecast_io_key  
+#end
+
 ForecastIO.configure do |config|    
-  config.api_key = Figaro.env.forecast_io_key  
+  config.api_key = ENV['FORECAST_IO_KEY']
 end
